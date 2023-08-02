b9ec45c798e29bda628d17684dfa3e116c16e15b
Simran Singh <simran.develop@gmail.com>
2023-08-03 08:39:17 +1000

    feat: Header component added

---
+++ src/pages/Header.svelte
@@ -0,0 +1,7 @@
+<header class="flex flex-col text-center my-5">
+  <a href="/">
+    <h1 class="text-5xl font-bold text-cyan-400 tracking-tighter">
+      AI Backgroud Remover
+    </h1>
+  </a>
+</header>
